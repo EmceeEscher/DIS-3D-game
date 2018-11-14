4273e62610ea1e525349b452e7a387ddb6e67de9
Aaron <anp36@duke.edu>
2018-11-14 16:44:46 +0100

    Basic monster ai

--- 3DGame/Assets/Scripts/SimplePatrol.cs
+++ 3DGame/Assets/Scripts/SimplePatrol.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FillerScript : MonoBehaviour {
+public class SimplePatrol : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
