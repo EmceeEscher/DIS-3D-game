de4588bfecbf3bf15b71ba7e61071dfd6cadf9ba
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-11-14 14:44:01 +0100

    added framework for object ripples

---
+++ 3DGame/Assets/Scripts/ObjectRippleHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ObjectRippleHandler : MonoBehaviour {
+
+    public RippleManager rippleManager;
+    public float maxVibrationTime = 5.0f;
+
+    Renderer renderer;
+    float currVibrationTime = 0.0f;
+    bool isVibrating = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.SetFloat("_VibrationProgress", 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Ripple> ripples = rippleManager.getRipples();
+        foreach (Ripple ripple in ripples) {
+            if (Mathf.Abs(calculateDistance(ripple) - ripple.currRadius) < ripple.thickness && !isVibrating) {
+                isVibrating = true;
+                currVibrationTime = 0.0f;
+            }
+        }
+
+        if (isVibrating) {
+            if (currVibrationTime > maxVibrationTime)
+            {
+                isVibrating = false;
+                renderer.material.SetFloat("_VibrationProgress", 0.0f);
+            }
+            else
+            {
+                currVibrationTime += Time.deltaTime;
+                renderer.material.SetFloat("_VibrationProgress", currVibrationTime / maxVibrationTime);
+            }
+        }
+    }
+
+    float calculateDistance(Ripple ripple) {
+        return Mathf.Sqrt(
+            Mathf.Pow((ripple.centerX - transform.position.x), 2) + 
+            Mathf.Pow((ripple.centerZ - transform.position.z), 2));
+    }
+}
