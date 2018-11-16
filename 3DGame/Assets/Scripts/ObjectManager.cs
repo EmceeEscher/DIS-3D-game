5a6380118060cc15723910347b1143dbed5a037f
Allen Gueco <allen.gueco1@gmail.com>
2018-11-16 10:26:53 +0100

    initial commit for throwing objects

---
+++ 3DGame/Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour {
+    // Show if can be picked up. Highlight?
+    // Listen for input. 
+    // Use this for initialization
+
+    private GameObject item;
+    public bool showVision = false;
+    public float visionDistance = 5F;
+    public LayerMask layerMask;
+
+    GameObject CheckInFront() {
+        RaycastHit hit;
+        // Raycast checking if there is an object (that has 'interactable' layer) from visiondistance, 
+        // highlight this object.
+        if (Physics.Raycast(transform.position, transform.forward, out hit, visionDistance, layerMask)) {
+            Debug.Log("Did hit");
+            Debug.DrawRay(transform.position, transform.forward * visionDistance, Color.yellow);
+
+            // Assumes that layer masks only selects objects that are throwable.
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Draws the ray if showVision is true. For debugging
+        if (showVision == true) {
+            Debug.DrawRay(transform.position, transform.forward * visionDistance, Color.red);
+        }
+
+        // Sets item to be whatever is interacting with the ray. If false, it's null.
+        item = CheckInFront();
+
+        if (Input.GetKey("E") && item.GetComponent("Pickable") {
+            item.Throw();
+        }
+
+    }
+}
---
+++ 3DGame/Assets/Scripts/Pickable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour {
+
+    private Collider col;
+    private Rigidbody rbdy;
+
+    private bool pickedUp = false;
+
+    // Communicates to the object that this object is now being hit by the Raycast
+    // (see CheckInFront method in ObjectManager)
+    public void OnPickup(Collider col, Rigidbody rbdy) {
+        pickedUp = true;
+
+        // Disable the collider and rigidbody when picked up.
+        col.enabled = !col.enabled;
+        rbdy.isKinematic = !rbdy.isKinematic;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        OnPickup(this.col, this.rbdy);
+
+        // transform this so it is in front of the player
+    }
+
+    public void Throw() {
+        
+    }
+
+    
+    // Use this for initialization
+    void Start () {
+        col = GetComponent<Collider>();
+        rbdy = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
