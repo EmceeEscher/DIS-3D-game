31284103be40b0d7641ffaf121d3dc8e529e4035
Julia <jfriend@oberlin.edu>
2018-11-24 21:44:48 +0100

    color input; new bubble obstacle not shading properly

--- 3DGame/Assets/Scripts/RippleScripts/ObjectRippleHandler.cs
+++ 3DGame/Assets/Scripts/RippleScripts/ObjectRippleHandler.cs
@@ -23,11 +23,22 @@ public class ObjectRippleHandler : MonoBehaviour {
     [Tooltip("Amplitude of sine curve of vibration.")]
     public float vibrationAmplitude = 3.0f;
 
+    [Tooltip("Controls for hue.")]
+    public float colorOffset = 0.5f;
+
     RippleManager rippleManager;
 
     protected Renderer renderer;
     protected float currVibrationTime = 0.0f;
     protected bool isVibrating = false;
+
+    private float Sigmoid(float x)
+    {
+        // Used to map colorOffset between 0 and 1
+        // @ https://www.robosoup.com/2008/09/sigmoid-function-in-c.html
+        float y = (float)(1 / (1 + Math.Exp(-x)));
+        return y * (1 - y);
+    }
 
     // Use this for initialization
     void Start()
@@ -41,6 +52,15 @@ public class ObjectRippleHandler : MonoBehaviour {
         renderer.material.SetFloat("_WidthOfVibration", vibrationWidth);
         renderer.material.SetFloat("_PeriodOfVibration", vibrationPeriod);
         renderer.material.SetFloat("_AmplitudeOfVibration", vibrationAmplitude);
+
+        /*renderer.material.SetFloat("_R", Sigmoid(3 * colorOffset)); // maybe not use simgoid here?
+        renderer.material.SetFloat("_G", Sigmoid(5 * colorOffset));
+        renderer.material.SetFloat("_B", Sigmoid(colorOffset));
+
+        if (Sigmoid(colorOffset) > 1 || Sigmoid(colorOffset) < 0) {
+            Debug.Log("Sigmoid does not work");
+        }*/
+        renderer.material.SetFloat("_Color", colorOffset);
 
         rippleManager = GameObject.FindWithTag("RippleManager").GetComponent<RippleManager>();
     }
