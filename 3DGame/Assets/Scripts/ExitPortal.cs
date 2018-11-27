84988bc11ba821169a094bf3fa37567d04f94b6b
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-11-27 13:50:21 +0100

    added fadeout after entering exit protal

--- 3DGame/Assets/Scripts/ExitPortal.cs
+++ 3DGame/Assets/Scripts/ExitPortal.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class ExitPortal : MonoBehaviour {
 
     GameObject player;
+    FadeoutManager fadeoutManager;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindWithTag("Player");
+        fadeoutManager = GameObject.FindWithTag("FadeoutManager").GetComponent<FadeoutManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(player.transform);
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(fadeoutManager.Fadeout());
+        }    
+    }
 }
---
+++ 3DGame/Assets/Scripts/FadeoutManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeoutManager : MonoBehaviour {
+
+    public Image fadeoutImage;
+    public float fadeoutTime = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator Fadeout()
+    {
+        Color currentColor = fadeoutImage.color;
+
+        Color visibleColor = fadeoutImage.color;
+        visibleColor.a = 1f;
+
+        float counter = 0f;
+
+        while (counter < fadeoutTime)
+        {
+            counter += Time.deltaTime;
+            fadeoutImage.color = Color.Lerp(currentColor, visibleColor, counter / fadeoutTime);
+            yield return null;
+        }
+
+        //TODO load scene with end credits after this
+    }
+}
