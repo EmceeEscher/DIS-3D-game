0c68b0ec24ef86a397f6e0b9ac1f3058d8497acd
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-11-27 11:02:19 +0100

    added goal object scripts and manager

---
+++ 3DGame/Assets/Scripts/GoalManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    public int numRequiredWaypoints = 4;
+
+    int numObtainedWaypoints = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (numObtainedWaypoints >= numRequiredWaypoints) {
+            Debug.Log("you completed the level!");
+        }
+	}
+
+    public void incrementObtainedWaypoints() {
+        numObtainedWaypoints++;
+    }
+}
