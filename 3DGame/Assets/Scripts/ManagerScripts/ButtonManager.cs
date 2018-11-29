7d9745c6256212c90c2826e3276e64e47723280a
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-11-29 15:45:18 +0100

    made play button load scene v1.2 for now

---
+++ 3DGame/Assets/Scripts/ManagerScripts/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadMainLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    public void ShowInstructions()
+    {
+
+    }
+}
