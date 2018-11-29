3f1ef1fde4b34d819f40d733cffac4bf04bce58b
Jacob Brunette <14202652+EmceeEscher@users.noreply.github.com>
2018-11-29 16:53:36 +0100

    Added a title screen (#18)
    * created title screen with placeholder graphics
    
    * made play button load scene v1.2 for now
    
    * made buttons work
    
    * added fonts and made credits work better
    
    * made buttons less ugly

---
+++ 3DGame/Assets/Scripts/TextPanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPanel : MonoBehaviour {
+
+    public GameObject nextPage;
+    public GameObject prevPage;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadPrevPage()
+    {
+        gameObject.SetActive(false);
+        prevPage.SetActive(true);
+    }
+
+    public void LoadNextPage()
+    {
+        gameObject.SetActive(false);
+        nextPage.SetActive(true);
+    }
+}
